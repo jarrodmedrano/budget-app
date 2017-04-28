473be4db39a4649c603b70a17f6f2934da28a9b2
jmedran <jmedran@gmail.com>
2017-04-27 20:04:08 -0700

    adding redux form

--- src/containers/expense-form.js
+++ src/containers/expense-form.js
@@ -1,22 +1,57 @@
 import React from 'react'
 import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
-import { reduxForm } from 'redux-form'
+import { Field, reduxForm } from 'redux-form'
 
 const submit = (values) => {
   console.log('submitting form', values)
 };
 
+const RenderInput = (props) => {
+  const { input, meta, ...inputProps } = props;
+
+  const formStates = ['active', 'autofilled', 'asyncValidating', 'dirty', 'invalid', 'pristine',
+    'submitting', 'touched', 'valid', 'visited'];
+
+
+  return (
+    <View>
+      <TextInput
+        {...inputProps}
+        onChangeText={input.onChange}
+        onBlur={input.onBlur}
+        onFocus={input.onFocus}
+        value={input.value}
+      />
+      <Text>The { input.name} input is:</Text>
+      {
+        formStates.filter((state) => meta[state]).map((state) => {
+          return <Text key={state}> - { state }</Text>;
+        })
+      }
+    </View>
+  );
+}
+
 const ExpenseForm = (props) => {
   const { handleSubmit } = props;
+
+  const formStates = ['active', 'autofilled', 'asyncValidating', 'dirty', 'invalid', 'pristine',
+    'submitting', 'touched', 'valid', 'visited'];
 
 
   return (
     <View style={styles.container}>
       <Text>Add a new expense</Text>
-      <TextInput style={styles.input} placeholder="Expense Name" />
-      <TextInput style={styles.input} placeholder="Expense Cost" />
-      <TextInput style={styles.input} placeholder="Expense Date" />
-      <TouchableOpacity onPress={handleSubmit(submit)}>
+      <Field style={styles.input} name={'name'} component={RenderInput} />
+      <Field style={styles.input} name={'cost'} placeholder="Expense Cost" component={RenderInput} />
+      <Field style={styles.input} name={'date'} placeholder="Expense Date" component={RenderInput} />
+      <Text>The form is:</Text>
+      {
+        formStates.filter((state) => props[state]).map((state) => {
+          return <Text key={state}> - { state }</Text>
+        })
+      }
+      <TouchableOpacity onPress={props.handleSubmit}>
         <Text style={styles.button}>Submit</Text>
       </TouchableOpacity>
     </View>
